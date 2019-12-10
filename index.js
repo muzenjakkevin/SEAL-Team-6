07c09d9a0f068faf6d27506886d062056b55605d
ArnT <anders_soderberg@hotmail.com>
2019-12-10 19:34:30 +0100

    Basic UI

---
+++ index.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+   initUI();
+});
+
+function initUI() {
+// linking body
+let myBody = document.getElementsByTagName('body')[0];
+
+// first div
+let firstDiv = document.createElement('div');
+myBody.appendChild(firstDiv)[0];
+firstDiv.id= 'firstDiv';
+
+// header
+let myHeader = document.createElement('h2');
+firstDiv.appendChild(myHeader);
+myHeader.innerHTML = 'Calendar application 2000';
+myHeader.style.margin = '0';
+
+// menu div
+let menuDiv = document.createElement('div');
+myBody.appendChild(menuDiv)[1];
+menuDiv.id = 'menyDiv';
+
+
+// first paragraph
+let myPara1 = document.createElement('p');
+menuDiv.appendChild(myPara1);
+myPara1.innerHTML = 'New activity';
+myPara1.id = 'myPara1';
+
+
+// second paragraph 
+let myPara2 = document.createElement('p');
+menuDiv.appendChild(myPara2);
+myPara2.innerHTML = 'Calendar';
+myPara2.id = 'myPara2';
+}
