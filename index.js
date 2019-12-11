c368ee29c1fe2408337df3050b51a0d525bac46c
ArnT <anders_soderberg@hotmail.com>
2019-12-11 18:01:03 +0100

    cleaned a bit

--- index.js
+++ index.js
@@ -3,76 +3,73 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function initUI() {
-// linking body
-let myBody = document.getElementsByTagName('body')[0];
+   // linking body
+   let myBody = document.getElementsByTagName('body')[0];
 
-// first div
-let firstDiv = document.createElement('div');
-myBody.appendChild(firstDiv)[0];
-firstDiv.id= 'firstDiv';
+   // first div
+   let firstDiv = document.createElement('div');
+   myBody.appendChild(firstDiv)[0];
+   firstDiv.id = 'firstDiv';
 
-// header
-let myHeader = document.createElement('h2');
-firstDiv.appendChild(myHeader);
-myHeader.innerHTML = 'Calendar application 2000';
-myHeader.style.margin = '0';
+   // header
+   let myHeader = document.createElement('h2');
+   firstDiv.appendChild(myHeader);
+   myHeader.innerHTML = 'Calendar application 2000';
+   myHeader.style.margin = '0';
 
-// menu div
-let menuDiv = document.createElement('div');
-myBody.appendChild(menuDiv)[1];
-menuDiv.id = 'menyDiv';
+   // menu div
+   let menuDiv = document.createElement('div');
+   myBody.appendChild(menuDiv)[1];
+   menuDiv.id = 'menyDiv';
 
+   // first paragraph
+   let myPara1 = document.createElement('p');
+   menuDiv.appendChild(myPara1);
+   myPara1.innerHTML = 'New activity';
+   myPara1.id = 'myPara1';
 
-// first paragraph
-let myPara1 = document.createElement('p');
-menuDiv.appendChild(myPara1);
-myPara1.innerHTML = 'New activity';
-myPara1.id = 'myPara1';
+   // second paragraph 
+   let myPara2 = document.createElement('p');
+   menuDiv.appendChild(myPara2);
+   myPara2.innerHTML = 'Calendar';
+   myPara2.id = 'myPara2';
 
-
-// second paragraph 
-let myPara2 = document.createElement('p');
-menuDiv.appendChild(myPara2);
-myPara2.innerHTML = 'Calendar';
-myPara2.id = 'myPara2';
-
-formInput()
+   formInput()
 }
-_____________________________________________________________________________________________________
+//_____________________________________________________________________________________________________
 
 function formInput() {
    let myBody = document.getElementsByTagName('body')[0]; // linked to body for now, change when navigation variables are known
-   let myForm = document.createElement('form');
+   let myForm = document.createElement('div');
    myBody.appendChild(myForm)[2];
    myForm.id = 'myForm';
-   myForm.style.color = 'white';
-   
+
    // New activity input
-   var inputText1 = document.createElement('p')
+   var inputText1 = document.createElement('p');
    inputText1.innerHTML = 'New activity:';
-   inputText1.style.margin = '0'
-   myForm.appendChild(inputText1)
+   inputText1.style.margin = '0';
+   myForm.appendChild(inputText1);
    var inputActivity = document.createElement('input');
    myForm.appendChild(inputActivity);
    inputActivity.type = 'text';
    inputActivity.name = 'New activity:';
    inputActivity.style.width = '166px';
-   inputActivity.className = 'input'
-   
+   inputActivity.className = 'input';
+
    // New activity date input
-   var inputText2 = document.createElement('p')
+   var inputText2 = document.createElement('p');
    inputText2.innerHTML = 'Date:';
-   myForm.appendChild(inputText2)
-   inputText2.style.margin = '0'
+   myForm.appendChild(inputText2);
+   inputText2.style.margin = '0';
    var inputDate = document.createElement('input');
    myForm.appendChild(inputDate);
    inputDate.type = 'text';
    inputDate.name = 'Date:';
    inputDate.style.width = '166px';
    inputDate.className = 'input';
-   
+
    // submit-button
-   var newActivity =  inputText1.innerText + inputText2.innerText;
+   var newActivity = inputDate.innerHTML + inputActivity.innerHTML;
    var submitBtn = document.createElement('button');
    myForm.appendChild(submitBtn);
    submitBtn.innerHTML = 'Add Activity';
@@ -80,9 +77,7 @@ function formInput() {
    submitBtn.style.marginTop = '17px';
    submitBtn.style.width = '100px';
    submitBtn.style.height = '30px';
-   
    submitBtn.addEventListener('click', function() {
-      console.log(newActivity);
-   })
-   }
-   
+      console.log('input')
+   });
+}
