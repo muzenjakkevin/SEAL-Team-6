49dcc14463715b9e08e6aa7f230cccdb335dc0c0
muzenjakkevin <muzenjak57@gmail.com>
2019-12-12 15:57:08 +0100

    Changed in js

--- index.js
+++ index.js
@@ -1,40 +1,46 @@
 document.addEventListener('DOMContentLoaded', function () {
-   initUI();
+   init();
 });
 
-function initUI() {
-// linking body
-let myBody = document.getElementsByTagName('body')[0];
-
-// first div
-let firstDiv = document.createElement('div');
-myBody.appendChild(firstDiv)[0];
-firstDiv.id= 'firstDiv';
-
-// header
-let myHeader = document.createElement('h2');
-firstDiv.appendChild(myHeader);
-myHeader.innerHTML = 'Calendar application 2000';
-myHeader.style.margin = '0';
-
-// menu div
-let menuDiv = document.createElement('div');
-myBody.appendChild(menuDiv)[1];
-menuDiv.id = 'menyDiv';
-
-// first paragraph
-let myPara1 = document.createElement('p');
-menuDiv.appendChild(myPara1);
-myPara1.innerHTML = 'New activity';
-myPara1.id = 'myPara1';
-
-// second paragraph 
-let myPara2 = document.createElement('p');
-menuDiv.appendChild(myPara2);
-myPara2.innerHTML = 'Calendar';
-myPara2.id = 'myPara2';
+function init() {
+  initUI();
+  addActivity();
 }
 
-function openActivity(){
-   var openNewscreen = null;
+function initUI(){
+  // linking body
+  let myBody = document.getElementsByTagName('body')[0];
+
+  // first div
+  let firstDiv = document.createElement('div');
+  myBody.appendChild(firstDiv)[0];
+  firstDiv.id= 'firstDiv';
+
+  // header
+  let myHeader = document.createElement('h2');
+  firstDiv.appendChild(myHeader);
+  myHeader.innerHTML = 'Calendar application 2000';
+  myHeader.style.margin = '0';
+
+  // menu div
+  let menuDiv = document.createElement('div');
+  myBody.appendChild(menuDiv)[1];
+  menuDiv.id = 'menyDiv';
+
+  // first paragraph
+  let myPara1 = document.createElement('p');
+  menuDiv.appendChild(myPara1);
+  myPara1.innerHTML = 'New activity';
+  myPara1.id = 'myPara1';
+  openActivity();
+
+  // second paragraph 
+  let myPara2 = document.createElement('p');
+  menuDiv.appendChild(myPara2);
+  myPara2.innerHTML = 'Calendar';
+  myPara2.id = 'myPara2';
+}
+
+function addActivity(){
+  myPara1.addEventListener('onclick', body.innerHTML = 'activity');
 }
